3a6ef1825b501ef129e14a1e9a9b16e6011f1932
mo7amedehab97 <gazacityface@gmail.com>
2022-12-05 20:16:19 +0200

    login page responsive

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -5,16 +5,6 @@ import facebookLogo from "../../assets/fslogo.svg";
 const LoginPage = () => {
   return (
     <div className="log_container">
-      <div className="log_form">
-        <h1 className="login_header">Log In To Facebook</h1>
-        <Input placeholder={"email or phone number"} className="login_input" />
-        <Input placeholder={"password"} className="login_input" />
-
-        <Button name={"Login"} className="login_btn" />
-        <Button name={"Do you forgot your password"} className="another_link" />
-        <hr className="newsection" />
-        <Button name={"create new account"} className="signup_btn" />
-      </div>
       <div className="log_info">
         <div className="log_holder">
           <img src={facebookLogo} alt="facbook logo" width={240} />
@@ -25,6 +15,16 @@ const LoginPage = () => {
             you want with them
           </p>
         </div>
+      </div>
+      <div className="log_form">
+        <h1 className="login_header">Log In To Facebook</h1>
+        <Input placeholder={"email or phone number"} className="login_input" />
+        <Input placeholder={"password"} className="login_input" />
+
+        <Button name={"Login"} className="login_btn" />
+        <Button name={"Do you forgot your password"} className="another_link" />
+        <hr className="newsection" />
+        <Button name={"create new account"} className="signup_btn" />
       </div>
     </div>
   );
